553cb27be5693a94a6022f7cd4184c4db2780c10
Thorsten <thorsten.wildberger@t-online.de>
2017-05-24 13:24:38 +0200

    Test project

---
+++ whiteboard.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Thorsten on 24.05.2017.
+ */
+
+var mousePressed = false;
+var lastX, lastY;
+var ctx;
+
+function InitThis() {
+    ctx = document.getElementById('whiteboard').getContext("2d");
+
+    $('#whiteboard').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#whiteboard').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#whiteboard').mouseup(function (e) {
+        mousePressed = false;
+    });
+    $('#whiteboard').mouseleave(function (e) {
+        mousePressed = false;
+    });
+}
+
+function Draw(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 5;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
