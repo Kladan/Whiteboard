5ba5d3d3c94ea0ce004bd6d58cf2f4a1d0108b64
kladan <adrian.klopfer@web.de>
2017-05-30 16:31:31 +0200

    Schnelles Einblenden des Boards beim Aufrufen der Seite Ein- und Ausklappen der Tools Impressum mit Lorem Ipsum gefüllt

--- whiteboard.js
+++ whiteboard.js
@@ -25,8 +25,13 @@ var ctx;
 function InitThis() {
     ctx = document.getElementById('whiteboard').getContext("2d");
 
-    ctx.canvas.style.width = '80%';
+    ctx.canvas.style.width = '0%';
     ctx.canvas.style.height = '70%';
+    ctx.canvas.style.left = '50%';
+    setTimeout(function(){
+        ctx.canvas.style.width = '80%';
+        ctx.canvas.style.left = '0%';
+    }, 0);
     ctx.canvas.width = ctx.canvas.offsetWidth;
     ctx.canvas.height = ctx.canvas.offsetHeight;
 
@@ -47,7 +52,6 @@ function InitThis() {
     $('#whiteboard').mouseleave(function (e) {
         mousePressed = false;
     });
-
 }
 
 function Draw(x, y, isDown) {
@@ -69,4 +73,17 @@ function clearArea() {
     // Use the identity matrix while clearing the canvas
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function tools() {
+    if (document.getElementById('tools').style.left == '0em') {
+        setTimeout(function() {
+            document.getElementById('tools').style.left = '-5em';
+        })
+    }
+    else {
+        setTimeout(function() {
+            document.getElementById('tools').style.left = '0em';
+        })
+    }
 }
