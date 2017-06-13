8be17bb172510772690ed54f028861f3a6efa522
kladan <adrian.klopfer@web.de>
2017-06-13 15:31:46 +0200

    whiteboard.sql

---
+++ db_config/whiteboard.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS whiteboard {
+	boardId int NOT NULL AUTO_INCREMENT,
+	created_date date NOT NULL,
+	created_by int NOT NULL,
+	drawing_data varbinary NOT NULL,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (created_by) REFERENCES user(userId)
+}
