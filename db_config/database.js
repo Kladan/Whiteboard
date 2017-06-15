5494aeda5e7b20067abc319d67aaa40c008108fb
Thorsten <thorsten.wildberger@t-online.de>
2017-06-15 12:03:50 +0200

    Skript zur Erstellung der DB; Sql-Dateien gelöscht und Inhalt in "create_database.js" gepackt

---
+++ db_config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'connection': {
+    	'host': 'localhost',
+    	'user': 'root',
+    	'password': 'test123'
+    },
+	'database': 'sketchbookDB',
+    'userTable': 'user',
+    'boardTable': 'whiteboard',
+    'sharedTable': 'shared'
+}
