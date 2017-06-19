bb574386f463f6fb26fce3714e65a9eaa2187526
kladan <adrian.klopfer@web.de>
2017-06-19 13:41:47 +0200

    Unicode Encoding in Tabellen eingefügt

--- db_config/create_database.js
+++ db_config/create_database.js
@@ -7,15 +7,17 @@ connection.query('CREATE DATABASE IF NOT EXISTS ' + dbConfig.database);
 
 
 connection.query("CREATE TABLE IF NOT EXISTS sketchbookdb.user ( `userId` int NOT NULL AUTO_INCREMENT, " +
-	"`username` VARCHAR(100) NOT NULL, `email` VARCHAR(200) NOT NULL, `password` CHAR(60) NOT NULL," +
+	"`username` VARCHAR(100) NOT NULL COLLATE utf8_unicode_ci, `email` VARCHAR(200) NOT NULL COLLATE utf8_unicode_ci, " +
+	"`password` CHAR(60) NOT NULL COLLATE utf8_unicode_ci," +
 	"\nPRIMARY KEY (userId)," +
 	"\nUNIQUE KEY `user_index` (`username`, `email`) );", function(error, results, fields){
 	if (error) throw error;
 });
 
 connection.query("CREATE TABLE IF NOT EXISTS sketchbookdb.whiteboard ( `boardId` int NOT NULL AUTO_INCREMENT, " +
-	"`created_date` date NOT NULL, `created_by` int NOT NULL, `drawing_data` LONGBLOB NOT NULL, " +
-	"`title` varchar(25), \nPRIMARY KEY (boardId), " +
+	"`created_date` date NOT NULL, `created_by` int NOT NULL, " +
+	"`drawing_data` LONGBLOB NOT NULL, " +
+	"`title` varchar(25) COLLATE utf8_unicode_ci, \nPRIMARY KEY (boardId), " +
 	"\nFOREIGN KEY (`created_by`) REFERENCES sketchbookdb.user(`userId`) );", function(error, results, fields){
 	if (error) throw error;
 });
@@ -27,6 +29,27 @@ connection.query("CREATE TABLE IF NOT EXISTS sketchbookdb.shared ( `sharedId` in
 	"\nFOREIGN KEY (boardId) REFERENCES sketchbookdb.whiteboard(boardId) );", function(error, results, fields){
 	if (error) throw error;
 });
+
+connection.query("ALTER DATABASE " + dbConfig.database + " DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci", 
+	function(error, result, fields){
+		if (error) throw error;
+});
+connection.query("ALTER TABLE " + dbConfig.database + "." + dbConfig.userTable +
+	" DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci", 
+	function(error, result, fields){
+		if (error) throw error;
+});
+connection.query("ALTER TABLE " + dbConfig.database + "." + dbConfig.boardTable +
+	" DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci", 
+	function(error, result, fields){
+		if (error) throw error;
+});
+connection.query("ALTER TABLE " + dbConfig.database + "." + dbConfig.sharedTable +
+	" DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci", 
+	function(error, result, fields){
+		if (error) throw error;
+});
+
 
 console.log("Database created!");
 
