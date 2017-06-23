a89710528c059ef180fcc6348fd358fec43ee414
Thorsten <thorsten.wildberger@t-online.de>
2017-06-23 16:16:54 +0200

    Routen Beispiel gelöscht; Prototyp für das board Modul

---
+++ app/board.js
@@ -0,0 +1,24 @@
+
+//Module laden
+//require mysql, database config
+//mysql connection erstellen
+//use sketchbookdb
+
+module.exports = {
+
+//Bild in Db speichern
+save: function(id) {
+
+},
+
+//Alle Whiteboards abfragen und als json zurückgeben
+getAll: function() {
+
+},
+
+//Mit Id zurückgeben
+getById: function(id) {
+
+}
+
+};
