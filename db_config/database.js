bc805fbbead5c284e1cdd324290b373ee32fda2d
Thorsten <thorsten.wildberger@t-online.de>
2017-07-07 19:29:38 +0200

    - Boards löschen; - Meldung das Sketch gespeichert wurde, wird wieder angezeigt

--- db_config/database.js
+++ db_config/database.js
@@ -4,6 +4,7 @@ module.exports = {
     	'host': 'localhost',
     	'user': 'root',
     	'password': ''
+    	'multipleStatements': true
     },
 	'database': 'sketchbookDB',
     'userTable': 'user',
